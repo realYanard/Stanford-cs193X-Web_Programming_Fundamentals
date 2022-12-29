9ca59d1e05dcbbe106f9bef90e1422b54e372e01
Christian Evrard AMBARA <christian.ambaracm@gmail.com>
2022-12-29 20:30:54 +0100

    Evil Extension completed

--- hw2-quiz-ext/evil-extension/page.js
+++ hw2-quiz-ext/evil-extension/page.js
@@ -10,8 +10,26 @@ const MATCH_LIST = {
 	'THEY\'RE': 'THERE'
 };
 
+
 function transformTextNodes(node) {
-  
+	if (node.nodeType == Node.TEXT_NODE) {
+		let textArray = node.textContent.split(' ');
+		let newTextArray = new Array();
+		for(let word of textArray){
+			for(let matchingWord in MATCH_LIST){
+				if(word.trim() == matchingWord){
+					word = word.replace(matchingWord, MATCH_LIST[matchingWord]);
+					break;
+				}
+			}
+			newTextArray.push(word);
+		}
+		node.textContent = newTextArray.join(' ');
+  } 
+  for (const child of node.childNodes) {
+	if(child.nodeName !== 'SCRIPT' && child.nodeName !== 'STYLE' )
+		transformTextNodes(child);
+  }
 }
 
 transformTextNodes(document.body);
